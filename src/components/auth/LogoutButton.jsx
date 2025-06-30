138d7ed11be128b4a944d0c59648ea6bb0cd049d
JeanPierre <cazeauxjeanpierre@gmail.com>
2025-06-30 13:21:44 +0200

    added tooltip and logged in user name to logout button

--- src/components/auth/LogoutButton.jsx
+++ src/components/auth/LogoutButton.jsx
@@ -5,11 +5,28 @@ import Image from "next/image";
 import { logout } from "@/services/auth";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import MsgModal from "../ui/MsgModal";
 
 export default function LogoutButton() {
   const router = useRouter();
   const [msg, setMsg] = useState(null);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const loggedInUser = useSelector((state) => state.user.user);
+  const handleMouseEnter = (e) => {
+    setShowTooltip(true);
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
   const handleLogout = async () => {
     const response = await logout();
     if (!response.result) {
@@ -18,25 +35,46 @@ export default function LogoutButton() {
     router.replace("/auth");
   };
   return (
-    <div className={styles.logout}>
-      <MsgModal
-        isOpen={msg != null}
-        setMsg={setMsg}
-        title="Logout Error"
-        message={msg}
-      />
-      <Image
-        className="pointer"
-        src={EGG_PATH}
-        alt="Twitter-like egg logo"
-        width={50}
-        height={50}
-        onClick={handleLogout}
-      />
-      <div>
-        <p className="font-text txt-small txt-primary-color txt-bold">John</p>
-        <p className="font-text txt-small txt-greyed-color">@JohnCena</p>
+    <>
+      <div className={styles.logout}>
+        <MsgModal
+          isOpen={msg != null}
+          setMsg={setMsg}
+          title="Logout Error"
+          message={msg}
+        />
+        <Image
+          className="pointer"
+          src={EGG_PATH}
+          alt="Twitter-like egg logo"
+          width={50}
+          height={50}
+          onClick={handleLogout}
+          onMouseEnter={handleMouseEnter}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+        />
+
+        <div>
+          <p className="font-text txt-small txt-primary-color txt-bold">
+            {loggedInUser.firstname}
+          </p>
+          <p className="font-text txt-small txt-greyed-color">
+            @{loggedInUser.username}
+          </p>
+        </div>
       </div>
-    </div>
+      {showTooltip && (
+        <div
+          className={`${styles.tooltip} txt-small font-text`}
+          style={{
+            left: mousePosition.x,
+            top: mousePosition.y - 40,
+          }}
+        >
+          Logout
+        </div>
+      )}
+    </>
   );
 }
